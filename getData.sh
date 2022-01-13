2bc385fa65d95fb2d0765dc577dc6e7c886a5758
Erik Solen <erik@solen.dev>
2022-01-12 22:20:42 -0500

    allow adding more than school district data like tracts

--- getData.sh
+++ getData.sh
@@ -3,11 +3,14 @@ set -e
 # usage: ./getData.sh GENZ2020/shp/cb_2020_25_unsd_500k.zip
 
 # Look for data over in https://www2.census.gov/geo/tiger/ 
+# Definitions and naming conventions https://www2.census.gov/geo/tiger/GENZ2020/2020_file_name_def.pdf
 
 DATA_DIR=data/`dirname $1`
+ZIP_FILE=`basename $1`
+SHAPE_FILE="${DATA_DIR}/${ZIP_FILE/.zip/.shp}"
 
-if [ -d "$DATA_DIR" ]; then
-  echo "Directory $DATA_DIR already exists, doing nothing."
+if [ -f "${SHAPE_FILE}" ]; then
+  echo "Shape file ${SHAPE_FILE} already exists, doing nothing."
   exit 1
 fi
 
